58cbc542a88460c53392c82e886d59468c2d2c1c
Melissa Hull <melissahull87@gmail.com>
2021-04-28 16:25:44 -0500

    section finished

---
+++ RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Recipe Discription', 'https://www.mtpr.org/sites/kufm/files/styles/x_large/public/201703/Stuffed-Cabbage_Steven-Depolo_CC-BY-20.jpg'),
+    new Recipe('Test Recipe', 'Test Recipe Discription', 'https://www.mtpr.org/sites/kufm/files/styles/x_large/public/201703/Stuffed-Cabbage_Steven-Depolo_CC-BY-20.jpg')
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
