9958ba53c5da2774553e7bfce816dc46449270db
Melissa Hull <melissahull87@gmail.com>
2021-05-05 20:51:01 -0500

    assignment beginning finished

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,11 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
     ContactsComponent,
     ContactDetailComponent,
     ContactListComponent,
+    ContactItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
