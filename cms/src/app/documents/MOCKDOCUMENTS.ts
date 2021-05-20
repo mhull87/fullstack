1fde7ba4cb6a86d7fad5455b45a9c8d24f17aa27
Melissa Hull <melissahull87@gmail.com>
2021-05-19 19:33:53 -0500

    tweaks

--- cms/src/app/documents/MOCKDOCUMENTS.ts
+++ cms/src/app/documents/MOCKDOCUMENTS.ts
@@ -4,6 +4,7 @@ export const MOCKDOCUMENTS: any[] = [
   {
     id: '1',
     name: 'CIT 425 - Data Warehousing',
+    description: 'About Data Warehousing',
     url: 'https://rkjdatawarehousing.wordpress.com/',
     children: [
       {
