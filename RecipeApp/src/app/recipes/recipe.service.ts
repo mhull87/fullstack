543fe23d485184f8e2a5412407d9870ec1cbf4f1
Melissa Hull <melissahull87@gmail.com>
2021-06-09 20:44:13 -0500

    recipe app

--- RecipeApp/src/app/recipes/recipe.service.ts
+++ RecipeApp/src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -13,7 +15,7 @@ export class RecipeService {
       'https://www.mtpr.org/sites/kufm/files/styles/x_large/public/201703/Stuffed-Cabbage_Steven-Depolo_CC-BY-20.jpg',
       [
         new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 1)
+        new Ingredient('French Fries', 25)
       ]),
     new Recipe(
       'Another Test Recipe',
@@ -37,6 +39,21 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
