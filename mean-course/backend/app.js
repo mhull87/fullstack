8165cab202c709e6a1dd0c4f7ea1fa13e572aad8
Melissa Hull <melissahull87@gmail.com>
2021-09-14 12:50:37 -0500

    finished

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -22,9 +22,10 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -36,9 +37,12 @@ app.use((req, res, next) => {
   );
   next();
 });
-
+*/
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
 
 module.exports = app;
